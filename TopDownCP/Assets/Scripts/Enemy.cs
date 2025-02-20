59832d7551878b156738e3c31ca4cc3fc0421f52
ITHUB\Student <qew>
2025-02-20 10:02:07 +0300

    1 Part done

--- TopDownCP/Assets/Scripts/Enemy.cs
+++ TopDownCP/Assets/Scripts/Enemy.cs
@@ -8,7 +8,7 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] private Transform _target;
 
-    private Vector2 _movement = Vector2.zero;
+    //private Vector2 _movement = Vector2.zero;
 
     private Rigidbody2D _rb;
 
@@ -17,12 +17,12 @@ public class Enemy : MonoBehaviour
         _rb = GetComponent<Rigidbody2D>();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.GetComponent<PlayerInput>() != null)
         {
-            //_rb.velocity = new Vector2(collision.transform.position.x, collision.transform.position.y);
-            _rb.velocity = new Vector2(_target.position.x, _target.position.y);
+            //_rb.velocity = new Vector2(_target.position.x, _target.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, _target.position, 0.05f);
         }
     }
 
